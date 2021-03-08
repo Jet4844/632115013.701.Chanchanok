356e35e3833f711cc4eae583517462af9642eeeb
Jet4844 <chanchanok_n@cmu.ac.th>
2021-03-08 22:41:44 +0700

    1

---
+++ week12/Student.java
@@ -0,0 +1,21 @@
+package week12;
+
+public class Student {
+    String Name;
+    Integer Age;
+    Double GPA;
+
+    Student(){
+
+    }
+
+    Student(String name,Integer age,Double gpa){
+        this.Name = name;
+        this.Age = age;
+        this.GPA = gpa;   
+        
+        
+    }
+    
+}
+
---
+++ week12/mainStudent.java
@@ -0,0 +1,28 @@
+package week12;
+
+public class mainStudent {
+    public static void main(String[] args) {
+        
+        Student std01 = new Student("Mark",18,4.00);
+        Student std02 = new Student("Jane",18,2.20);
+        Student std03 = new Student("Peter",19,4.00);
+        Student std04 = new Student("Mark",22,1.75);
+
+
+        System.out.println("---------------Name----------------");
+        System.out.println("This is "+std01.Name.equals(std02.Name));
+        System.out.println("This is "+std01.Name.equals(std03.Name));
+        System.out.println("This is "+std02.Name.equals(std04.Name));
+        System.out.println("This is "+std01.Name.equals(std04.Name));
+        System.out.println("----------------age----------------");
+        System.out.println("This is "+std01.Age.equals(std02.Age));
+        System.out.println("This is "+std03.Age.equals(std04.Age));
+        System.out.println("This is "+std01.Age.equals(std03.Age));
+        System.out.println("----------------gpa----------------");
+        System.out.println("This is "+std01.GPA.equals(std03.GPA));
+        System.out.println("This is "+std02.GPA.equals(std04.GPA));
+        System.out.println("This is "+std02.GPA.equals(std03.GPA));
+    }
+
+
+    }
